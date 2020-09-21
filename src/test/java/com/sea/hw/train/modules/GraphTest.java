084519ad327a4362a0d0db0fcafe3bab6012501d
yichi <yichi.zhang@thoughtworks.com>
2020-09-21 23:30:16 +0800

    [yichi] create graph entity

---
+++ src/test/java/com/sea/hw/train/modules/GraphTest.java
@@ -0,0 +1,38 @@
+package com.sea.hw.train.modules;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+    private ArrayList<Vertex> vertices;
+    private ArrayList<Edge> edges;
+
+    @BeforeEach
+    void setUp(){
+        vertices = new ArrayList<Vertex>();
+        edges = new ArrayList<>();
+
+        Vertex vertexA = new Vertex("A");
+        Vertex vertexB = new Vertex("B");
+        Vertex vertexC = new Vertex("C");
+
+        vertices.add(vertexA);
+        vertices.add(vertexB);
+        vertices.add(vertexC);
+
+        edges.add(new Edge("Edge_0", vertexA, vertexB, 5));
+        edges.add(new Edge("Edge_1", vertexB, vertexC, 6));
+    }
+
+    @Test
+    void shouldConstructCorrectGraph() {
+        Graph graph = new Graph(vertices, edges);
+
+        assertEquals(edges, graph.getEdges());
+        assertEquals(vertices, graph.getVertexes());
+    }
+}
