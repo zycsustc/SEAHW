d9574013a73567bbf755783b8f6e18056612f0c5
yichi <yichi.zhang@thoughtworks.com>
2020-09-21 22:47:19 +0800

    [yichi] create edge entity

---
+++ src/test/java/com/sea/hw/train/modules/EdgeTest.java
@@ -0,0 +1,20 @@
+package com.sea.hw.train.modules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EdgeTest {
+    @Test
+    void shouldConstructCorrectEdge() {
+        Vertex source = new Vertex("A");
+        Vertex target = new Vertex("B");
+        int weight = 5;
+        String id = "id";
+        Edge edge = new Edge(id, source, target, weight);
+
+        assertEquals(weight, edge.getWeight());
+        assertEquals(source, edge.getSource());
+        assertEquals(target, edge.getDestination());
+    }
+}
