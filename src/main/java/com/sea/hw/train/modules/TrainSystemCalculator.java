655cd21ee1aba2abef5c5594b08ade5ccec0f39b
yichi <yichi.zhang@thoughtworks.com>
2020-09-22 13:14:54 +0800

    [yichi] Create TrainSystemCalculator that can calculate the distance of a given path

---
+++ src/main/java/com/sea/hw/train/modules/TrainSystemCalculator.java
@@ -0,0 +1,60 @@
+package com.sea.hw.train.modules;
+
+import java.util.LinkedList;
+
+public class TrainSystemCalculator {
+    private final Graph graph;
+
+    public TrainSystemCalculator(Graph graph) {
+        this.graph = graph;
+    }
+
+    public int getDistanceByPath(LinkedList<Vertex> path) {
+        int distance = 0;
+        int vertexIndex = 0;
+        Vertex nextVertex;
+        if (path.size() < 2) {
+            return distance;
+        }
+        for (Vertex vertex : path) {
+            nextVertex = path.get(vertexIndex + 1);
+            distance += getDistanceBetweenTwoVertices(vertex, nextVertex);
+            vertexIndex += 1;
+            if (vertexIndex == path.size() - 1) {
+                return distance;
+            }
+        }
+        return distance;
+    }
+
+    public String getExactPath(LinkedList<Vertex> exactPath) {
+        for (Vertex vertex : exactPath) {
+            if (vertex.equals(exactPath.getLast())) {
+                break;
+            } else if (getDistance(vertex, exactPath.get(exactPath.indexOf(vertex) + 1)) == Integer.MAX_VALUE) {
+                return "NO SUCH ROUTE";
+            }
+        }
+        return "ROUTE FIND";
+    }
+
+    private int getDistance(Vertex vertex, Vertex target) {
+        for (Edge edge : graph.getEdges()) {
+            if (edge.getSource().equals(vertex)
+                    && edge.getDestination().equals(target)) {
+                return edge.getWeight();
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    private int getDistanceBetweenTwoVertices(Vertex vertex, Vertex target) {
+        for (Edge edge : graph.getEdges()) {
+            if (edge.getSource().equals(vertex)
+                    && edge.getDestination().equals(target)) {
+                return edge.getWeight();
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+}
---
+++ src/test/java/com/sea/hw/train/modules/TrainSystemCalculatorTest.java
@@ -0,0 +1,69 @@
+package com.sea.hw.train.modules;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainSystemCalculatorTest {
+    private Graph graph;
+    private TrainSystemCalculator trainSystemCalculator;
+
+    @BeforeEach
+    void setUp() {
+        ArrayList<Vertex> vertexes = new ArrayList<>();
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        Vertex vertexA = new Vertex("A");
+        Vertex vertexB = new Vertex("B");
+        Vertex vertexC = new Vertex("C");
+        Vertex vertexD = new Vertex("D");
+        Vertex vertexE = new Vertex("E");
+
+        vertexes.add(vertexA);
+        vertexes.add(vertexB);
+        vertexes.add(vertexC);
+        vertexes.add(vertexD);
+        vertexes.add(vertexE);
+
+        edges.add(new Edge("Edge_0", vertexA, vertexB, 5));
+        edges.add(new Edge("Edge_1", vertexB, vertexC, 4));
+        edges.add(new Edge("Edge_2", vertexC, vertexD, 8));
+        edges.add(new Edge("Edge_3", vertexD, vertexC, 8));
+        edges.add(new Edge("Edge_4", vertexD, vertexE, 6));
+        edges.add(new Edge("Edge_5", vertexA, vertexD, 5));
+        edges.add(new Edge("Edge_6", vertexC, vertexE, 2));
+        edges.add(new Edge("Edge_7", vertexE, vertexB, 3));
+        edges.add(new Edge("Edge_8", vertexA, vertexE, 7));
+
+        graph = new Graph(vertexes, edges);
+        trainSystemCalculator = new TrainSystemCalculator(graph);
+    }
+
+    @Test
+    void shouldReturnPathDistanceExactlyAtoBtoC() {
+        ArrayList<String> stops = new ArrayList<>();
+        stops.add("A");
+        stops.add("B");
+        stops.add("C");
+        LinkedList<Vertex> path = graph.getLinkedVertexByStopsInGraph(stops);
+
+        assertEquals(trainSystemCalculator.getExactPath(path), "ROUTE FIND");
+        assertEquals(trainSystemCalculator.getDistanceByPath(path), 9);
+    }
+
+    @Test
+    void shouldReturnPathDistanceExactlyAtoD() {
+        ArrayList<String> stops = new ArrayList<>();
+        stops.add("A");
+        stops.add("D");
+        LinkedList<Vertex> path = graph.getLinkedVertexByStopsInGraph(stops);
+
+        assertEquals(trainSystemCalculator.getExactPath(path), "ROUTE FIND");
+        assertEquals(trainSystemCalculator.getDistanceByPath(path), 5);
+    }
+
+}
